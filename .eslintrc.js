001e1c0e85786dfc28e1d5e5b4753bc2dc8fbe86
John Campbell <johncampbell@geospatialweb.ca>
2018-12-11 17:35:46 -0500

    updated .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,10 @@ module.exports = {
 	},
 	rules: {
 		indent: ['error', 'tab'],
-		'import/prefer-default-export': false,
 		'linebreak-style': ['error', 'windows'],
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-param-reassign': ['error', { props: false }],
 		'no-tabs': ['error', { allowIndentationTabs: true }],
-		'no-underscore-dangle': 'off',
 	},
 };
