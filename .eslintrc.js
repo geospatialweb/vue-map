8f7e524a46ce571559869425fad952ad28b62de1
John Campbell <johncampbell@geospatialweb.ca>
2018-12-12 15:12:23 -0500

    updated .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,14 +11,18 @@ module.exports = {
 		parser: 'babel-eslint',
 	},
 	rules: {
-		'indent': ['error', 'tab'],
+		indent: ['error', 'tab'],
 		'linebreak-style': ['error', 'windows'],
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-param-reassign': ['error', { props: false }],
 		'no-tabs': ['error', { allowIndentationTabs: true }],
-		'no-unused-expressions': ['error', { 'allowTernary': true }],
-        'operator-linebreak': ['error', 'before',
-            { 'overrides': { '+': 'after', '?': 'after', ':': 'after', '&&': 'after' } }],
+		'no-unused-expressions': ['error', { allowTernary: true }],
+		'operator-linebreak': ['error', 'before',
+			{
+				overrides: {
+					'+': 'after', '?': 'after', ':': 'after', '&&': 'after',
+				},
+			}],
 	},
 };
