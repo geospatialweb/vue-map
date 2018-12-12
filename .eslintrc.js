e4d9b9bc43b23134a749d1228ac73e805da93867
John Campbell <johncampbell@geospatialweb.ca>
2018-12-12 13:01:32 -0500

    updated .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
 		parser: 'babel-eslint',
 	},
 	rules: {
-		indent: ['error', 'tab'],
+		'indent': ['error', 'tab'],
 		'linebreak-style': ['error', 'windows'],
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
